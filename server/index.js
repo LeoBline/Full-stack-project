ac025086c31c0d250d54376ca5dd587e5efeaee2
YXK <2581570217@qq.com>
2024-01-28 22:41:44 +0800

    Second commit

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const db = require('./models');
+
+//Routers
+const postRouter = require('./rountes/Posts');
+app.use("/posts",postRouter);
+
+db.sequelize.sync().then(() =>{
+  app.listen(3001,() =>{
+      console.log("Server running on port 3001"); 
+  });
+});
+
