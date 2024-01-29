8c51dd74bc64d5b37040d0f5b3fd1cdedc6888dc
YXK <2581570217@qq.com>
2024-01-29 10:28:47 +0800

    course 4

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
 import "./App.css";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
-  const [listOfPosts, setListOfPosts] = useState([]);
-
-  useEffect(() => {
-    axios.get("http://localhost:3001/posts").then((response) => {
-      console.log(response.data);
-      setListOfPosts(response.data);
-    });
-  }, []);
   return (
     <div className="App">
-      {" "}
-      {listOfPosts.map((value, key) => {
-        return (
-          <div className="post">
-            <div className="title">{value.title}</div>
-            <div className="body">{value.postText}</div>
-            <div className="footer">{value.username}</div>
-          </div>
-        );
-      })}{" "}
+      <Router>
+        <Link to="/"> Home Page </Link>
+        <Link to="/createpost"> Create A Post </Link>
+        <Routes>
+          <Route path="/" exact Component={Home} />
+          <Route path="/createpost" exact Component={CreatePost} />
+        </Routes>
+      </Router>
     </div>
   );
 }
